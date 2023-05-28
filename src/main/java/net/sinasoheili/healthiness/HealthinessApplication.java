2519761e3449fd7a987ef3759c314cc6cfad2acc
Sina <sinasoheili79@gmail.com>
2023-05-28 08:25:07 +0330

    Start Project

---
+++ src/main/java/net/sinasoheili/healthiness/HealthinessApplication.java
@@ -0,0 +1,13 @@
+package net.sinasoheili.healthiness;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthinessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthinessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/sinasoheili/healthiness/HealthinessApplicationTests.java
@@ -0,0 +1,13 @@
+package net.sinasoheili.healthiness;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthinessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
